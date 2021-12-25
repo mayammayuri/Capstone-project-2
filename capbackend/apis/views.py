5f4ba4ba57deda00500a6fc3aa8e32d6ea0b59fd
Shreyans Jain <shreyansjain2000@hotmail.com>
2021-12-25 12:58:57 +0530

    arrowfixes

--- capbackend/apis/views.py
+++ capbackend/apis/views.py
@@ -13,7 +13,7 @@ def apple_stock_api(request):
 def adidas_stock_api(request):
     loaded=sarimax.SARIMAXResultsWrapper.load('apis/arimaModels/adidas.pkl')
     predictions = loaded.predict(start=77,end=(85)+3,typ='levels')
-    data={'predictions':list(predictions),'accuracy':'96.45%','labels':['April 2021','May 2021','June 2021','July 2021','August 2021','September 2021','October 2021','November 2021','December 2021','January 2022','Feburary 2022','March 2022'],'cur':'$', 'name':'Adidas/ADDYY', 'current_price':'$175.13', 'predicted_price':'$170.91', 'up_down_current':'fas fa-arrow-down text-failure', 'up_down_predicted':'fas fa-arrow-down text-failure', 'up_down_current_since':'$2.66', 'predicted_current':'2.47%'}
+    data={'predictions':list(predictions),'accuracy':'96.45%','labels':['April 2021','May 2021','June 2021','July 2021','August 2021','September 2021','October 2021','November 2021','December 2021','January 2022','Feburary 2022','March 2022'],'cur':'$', 'name':'Adidas/ADDYY', 'current_price':'$175.13', 'predicted_price':'$170.91', 'up_down_current':'fas fa-arrow-down text-danger', 'up_down_predicted':'fas fa-arrow-down text-danger', 'up_down_current_since':'$2.66', 'predicted_current':'2.47%'}
     return JsonResponse(data)
 
 def zara_stock_api(request):
